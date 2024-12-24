9883a2db8e0dd78116adee7e92bbf58898faad38
Alextrade <40890651+Alextrade@users.noreply.github.com>
2024-12-24 14:57:02 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { APP_URL, PORT, TELEGRAM_TOKEN } from "./constants";
 import express, { Application } from "express";
 import { Telegraf } from "telegraf";
 
-const bot = new Telegraf(7581493084:AAEinIAFfy_UmawZ0fI1eqasE2Bs8AXbmkU);
+const bot = new Telegraf(TELEGRAM_TOKEN);
 const app: Application = express();
 
 app.use(express.static("static"));
@@ -13,18 +13,18 @@ app.get("/", (_, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is Fire at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 bot.command("start", (ctx) => {
-  return ctx.reply("Play RumVal!", {
+  return ctx.reply("Play RumVall", {
     reply_markup: {
       inline_keyboard: [
         [
-          { text: "Play Game", web_app: { url: `${APP_URL}` } },
-        ],
-      ],
-    },
+          { text: "Play Game", web_app: { url: `${APP_URL}` } }
+        ]
+      ]
+    }
   });
 });
 
